39ed74ff393950e81b31a3cd37335ce5f4862e03
Greg Hinch <greg@greghinch.com>
2013-02-21 16:34:32 -0800

    adding utility methods for adding a deal and getting basic data via name

--- pipedrive/__init__.py
+++ pipedrive/__init__.py
@@ -69,3 +69,109 @@ class Pipedrive(object):
 
             return response
         return wrapper
+
+    def get_organization_data(self, organization_name):
+        organization = self.organizations_find(params={'term' : organization_name})
+        if not 'data' in organization:
+            return None
+        
+        return organization['data'][0]
+
+    def get_person_data(self, person_name):
+        person = self.persons_find(params={'term' : person_name})
+        if not 'data' in person:
+            return None
+        return person['data'][0]
+
+    def get_pipeline_data(self, pipeline_name=None):
+        pipeline = None
+        pipelines = self.get_pipelines()
+
+        if 'data' in pipelines:
+            pipeline = pipelines['data'][0]
+
+            if pipeline_name is not None:
+                for pipe in pipelines['data']:
+                    if pipe['name'] == pipeline_name:
+                        pipeline = pipe
+                        break
+
+        return pipeline
+
+    def get_stage_data(self, stage_name=None, pipeline_id=None):
+        stage = None
+        params = dict()
+        if pipeline_id is not None:
+            params['pipeline_id'] = pipeline_id
+        stages = self.get_stages(params=params)
+        if 'data' in stages:
+
+            stage = stages['data'][0]
+            if stage_name is not None:
+                for st in stages['data']:
+                    if st['name'] == stage_name:
+                        stage = st
+                        break
+        return stage
+
+    def add_deal(self, deal_name, prepared_data, pipeline_name, person_name, organization_name, stage_name=None):
+
+        organization = self.get_organization_data(organization_name)
+        person = self.get_person_data(person_name)
+        pipeline = self.get_pipeline_data(pipeline_name)
+        stage = self.get_stage_data(stage_name, pipeline_id=(pipeline['id'] if pipeline else None))
+
+        if not organization or not person or not pipeline or not stage:
+            return False
+
+        pd_fields = self.get_dealFields()
+        if not 'data' in pd_fields:
+            return False
+
+        pd_fields = pd_fields['data']
+
+        field_pairs = dict()
+        field_options = dict()
+        field_names = prepared_data.keys()
+        for field in pd_fields:
+            if field['name'] in field_names:
+                field_pairs[field['name']] = field['key']
+
+                if 'options' in field and field['options']:
+                    field_options[field['name']] = field['options']
+
+        deal_data = {
+            'title' : deal_name,
+            'person_id' : person['id'],
+            'org_id' : organization['id'],
+            'stage_id' : stage['id'],
+        }
+
+        def get_option_id(options, value):
+            for opt in options:
+                if opt['label'] == value:
+                    return opt['id']
+            
+            return None
+
+        for key, field_key in field_pairs.items():
+            if key in prepared_data:
+                raw_val = prepared_data[key]
+                if key in field_options:
+                    if isinstance(raw_val, list):
+                        value = [get_option_id(field_options[key], val_item) for val_item in raw_val]
+                    else:
+                        value = get_option_id(field_options[key], raw_val)
+                else:
+                    value = unicode(raw_val).encode("utf-8")
+
+                deal_data[field_key] = value
+            else:
+                print key
+
+        deal = self.post_deals(data=deal_data)
+
+        if not 'error' in deal:
+            return deal
+        else:
+            return False
